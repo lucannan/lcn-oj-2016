492426c3f80509f67c2ce686c5f6aaebd474ab49
samuellcn <812481699@qq.com>
2016-04-05 13:32:35 +0800

    八数码问题     bfs

---
+++ src/main/java/CN/EDU/SEU/BFSandDFS/EightPuzzle.java
@@ -0,0 +1,166 @@
+package CN.EDU.SEU.BFSandDFS;
+
+import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * Created by LCN on 2016/4/4.
+ */
+public class EightPuzzle {
+    private String targetState = "123456780"; //目标状态
+    private int[][] targetMatrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};//目标矩阵
+    private Set<String> hashState = new HashSet<>();
+    private int[] dx = {-1, 0, 1, 0};
+    private int[] dy = {0, 1, 0, -1};
+    private Map<String, String> path = new HashMap<>();
+
+    private int step = 0;
+
+    //    private int[] dy = {}
+    private String convertToStrState(int[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                sb.append(matrix[i][j]);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private int[][] convertToMatrix(String state) {
+        int n = 3;
+        int[][] matrix = new int[3][3];
+        for (int i = 0; i < state.length(); i++) {
+            matrix[i / n][i % n] = state.charAt(i) - '0';
+        }
+        return matrix;
+    }
+
+    //求状态矩阵去除0后的逆序数
+    public int countInverseNumber(int[][] matrix) {
+        int[] temElem = new int[9];
+        int size = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrix[i][j] != 0) {
+                    temElem[size++] = matrix[i][j];
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (temElem[i] > temElem[j]) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean isCanSolve(int[][] startMatrix) {
+        return countInverseNumber(startMatrix) % 2 == countInverseNumber(targetMatrix);
+    }
+
+    public void bfs(int[][] startState) {
+        if(!isCanSolve(startState))
+        {
+            System.out.println("开始状态到目标状态无解！");
+        }
+
+        Queue<String> queue = new LinkedBlockingDeque<>();
+        queue.add(convertToStrState(startState));
+        hashState.add(convertToStrState(startState));
+        path.put(convertToStrState(startState), null);
+
+        while (!queue.isEmpty()) {
+            String currentState = queue.poll();
+            int[][] currentMatrix = convertToMatrix(currentState);
+            if (currentState.equals(targetState)) {
+                break;
+            }
+
+            int curx = 0;
+            int cury = 0;
+            //查找0的位置
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (currentMatrix[i][j] == 0) {
+                        curx = i;
+                        cury = j;
+                        break;
+                    }
+                }
+            }
+
+            String newState = ""; //记录新的状态
+            int[][] newMatrix = new int[3][3];
+
+            for (int i = 0; i < 4; i++) {
+                int newx = curx + dx[i];
+                int newy = cury + dy[i];
+
+                if (newx <= 2 && newx >= 0 && newy <= 2 && newy >= 0) {
+                    for (int j = 0; j < 3; j++) {
+                        System.arraycopy(currentMatrix[j], 0, newMatrix[j], 0, currentMatrix[j].length);
+                    }
+                    int temp = newMatrix[newx][newy];
+                    newMatrix[newx][newy] = newMatrix[curx][cury];
+                    newMatrix[curx][cury] = temp;
+
+                    newState = convertToStrState(newMatrix);
+                    if (!hashState.contains(newState)) {
+                        path.put(newState, currentState);
+                        queue.add(newState);
+                        hashState.add(newState);
+                    }
+                }
+            }
+        }
+    }
+
+
+    public void printPath() {
+        Stack<String> stack = new Stack<>();
+        String state = targetState;
+        while (state != null) {
+            stack.push(state);
+            state = path.get(state);
+            step++;
+        }
+        System.out.println("一共走了" + (step - 1) + "步");
+        while (!stack.isEmpty()) {
+            printMatrix(convertToMatrix(stack.pop()));
+        }
+    }
+
+    public void printMatrix(int[][] matrix) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        EightPuzzle eightPuzzle = new EightPuzzle();
+        int[][] startMatrix = new int[3][3];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                startMatrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        eightPuzzle.bfs(startMatrix);
+        eightPuzzle.printPath();
+
+    }
+
+}
--- src/test/java/CN/EDU/SEU/TenXunTest.java
+++ src/test/java/CN/EDU/SEU/TenXunTest.java
@@ -6,35 +6,34 @@ package CN.EDU.SEU;
 public class TenXunTest {
 
     public static int dfs(int i, int j, int[][] arr, int[][] cacheData) {
-        if (i == arr[0].length) {
+        if (i == arr.length) {
             return Integer.MIN_VALUE;
         }
-        if (j == arr.length) {
+        if (j == arr[0].length) {
             return Integer.MIN_VALUE;
         }
-        if (i == arr[0].length - 1 && j == arr.length - 1) {
+        if (i == arr.length - 1 && j == arr[0].length - 1) {
             return arr[i][j];
         }
         if (cacheData[i][j] != -1){
             return cacheData[i][j];
         }
 
-        int retLeft = dfs(i + 1, j, arr,cacheData);//右
-        int retRight = dfs(i, j + 1, arr,cacheData);  //下
+        int retRight = dfs(i + 1, j, arr,cacheData);    //右
+        int retDown = dfs(i, j + 1, arr,cacheData);     //下
 
-        if (retLeft < retRight) {
-            cacheData[i][j] = arr[i][j] + retRight;
-            return cacheData[i][j];
+        if (retRight < retDown) {
+            cacheData[i][j] = arr[i][j] + retDown;
         } else {
-            cacheData[i][j] = arr[i][j] + retLeft;
-            return cacheData[i][j];
+            cacheData[i][j] = arr[i][j] + retRight;
         }
+        return cacheData[i][j];
     }
 
     public static void main(String[] args) {
-        int[][] arrdata = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, -1}};
-        int[][] arrdata2 = {{0, 0, 3, 0}, {0, 0, 3, 4}, {0, 1, 1, 0}, {0, 0, 0, 4}};
-        int[][] cacheData = new int[4][4];
+        int[][] arrdata = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, -1},{1, 2, 3, -1}};
+//        int[][] arrdata2 = {{0, 0, 3, 0}, {0, 0, 3, 4}, {0, 1, 1, 0}, {0, 0, 0, 4}};
+        int[][] cacheData = new int[5][4];
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -44,11 +43,11 @@ public class TenXunTest {
 
 
         System.out.println(dfs(0,0,arrdata,cacheData));
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                cacheData[i][j] = -1;
-            }
-        }
-        System.out.println(dfs(0,0,arrdata2,cacheData));
+//        for (int i = 0; i < 4; i++) {
+//            for (int j = 0; j < 4; j++) {
+//                cacheData[i][j] = -1;
+//            }
+//        }
+//        System.out.println(dfs(0,0,arrdata2,cacheData));
     }
 }
