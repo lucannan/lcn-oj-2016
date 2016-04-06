5aa09f5b32517857acbe435099b08aa74e02f4c8
samuellcn <812481699@qq.com>
2016-04-06 15:12:36 +0800

    leetcode 133 clone graph

---
+++ src/main/java/CN/EDU/SEU/BFSandDFS/leetcode__133.java
@@ -0,0 +1,40 @@
+package CN.EDU.SEU.BFSandDFS;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by LCN on 2016/4/6.
+ */
+public class leetcode__133 {
+
+}
+
+class UndirectedGraphNode {
+    int label;
+    List<UndirectedGraphNode> neighbors;
+
+    UndirectedGraphNode(int x) {
+        label = x;
+        neighbors = new ArrayList<UndirectedGraphNode>();
+    }
+}
+
+class Solution {
+    HashMap<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap<>();
+
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        if (!map.containsKey(node)) {
+            int label = node.label;
+            List<UndirectedGraphNode> neighbors = node.neighbors;
+            List<UndirectedGraphNode> newNeighbors = new ArrayList<>();
+            UndirectedGraphNode newNode = new UndirectedGraphNode(label);
+            map.put(node, newNode);
+            for (UndirectedGraphNode cur : neighbors) newNeighbors.add(cloneGraph(cur));
+            newNode.neighbors = newNeighbors;
+        }
+        return map.get(node);
+    }
+}
