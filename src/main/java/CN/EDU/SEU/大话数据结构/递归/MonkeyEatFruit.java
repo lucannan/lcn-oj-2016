c1bbd9f6e07e74c7bd1a459e766f94dc56f29f3f
samuellcn <812481699@qq.com>
2016-04-13 09:06:48 +0800

    简单递推   猴子吃桃

--- src/main/java/CN/EDU/SEU/大话数据结构/递归/MonkeyEatFruit.java
+++ src/main/java/CN/EDU/SEU/大话数据结构/递归/MonkeyEatFruit.java
@@ -1,21 +1,46 @@
 package CN.EDU.SEU.大话数据结构.递归;
 
+import org.junit.Test;
+
 /**
  * 简单递归  monkey eat fruit
  * 极客学院
  * Created by LCN on 2016/4/13.
  */
 public class MonkeyEatFruit {
-
-    public static int eat01(int n){
+    //递推算法
+    public int eat01(int n) {
         int a = 1;
         for (int i = 2; i <= n; i++) {
-            a = 2*a + 1;
+            a = 2 * a + 1;
         }
         return a;
     }
 
+    //递归算法
+    public int eat02(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return 2 * eat02(n - 1)  + 2;
+    }
+
     public static void main(String[] args) {
-        System.out.println(eat01(10));
+//        System.out.println(eat01(10));
+    }
+
+    @Test
+    public void test01() {
+        int n = 10;
+        int num = eat01(n);
+
+    }
+
+    @Test
+    public void test02(){
+        int n = 10;
+        int num = eat02(n);
+        System.out.println(num);
     }
 }
