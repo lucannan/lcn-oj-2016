45b0fe2b6c52660d2ed8a0200e649f522c193b48
samuellcn <812481699@qq.com>
2016-04-13 22:21:34 +0800

    麦克劳林迭代

--- src/main/java/CN/EDU/SEU/极客学院名企面试/二分法/Maclaurin.java
+++ src/main/java/CN/EDU/SEU/极客学院名企面试/二分法/Maclaurin.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 /**
  * 计算自然对数e
+ * 计算pi
  * Created by LCN on 2016/4/13.
  */
 public class Maclaurin {
@@ -20,10 +21,31 @@ public class Maclaurin {
         return e;
     }
 
+    //计算pi
+    public double getPI(double n) {
+        double a = Math.sqrt(3) / 3;
+        double pi = a;
+        for (double i = 1; i <= n; i++) {
+            a = -a / 3;
+            pi += a / (2 * i + 1);
+        }
+
+        pi *= 6;
+        return pi;
+
+    }
+
     @Test
-    public void test01(){
+    public void test01() {
         double e = getE(100);
         System.out.println(e);
         System.out.println(Math.E);
+    }
+
+    @Test
+    public void testPi() {
+        double pi = getPI(100);
+        System.out.println(pi);
+        System.out.println(Math.PI);
     }
 }
