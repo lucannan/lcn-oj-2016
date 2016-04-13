70aa265669fe18db97a7a1b400aef1ae05cdde54
samuellcn <812481699@qq.com>
2016-04-13 13:31:03 +0800

    leetcode 70 爬楼梯

--- src/main/java/CN/EDU/SEU/leetcode/__70ClimbingStairs.java
+++ src/main/java/CN/EDU/SEU/leetcode/__70ClimbingStairs.java
@@ -19,17 +19,37 @@ public class __70ClimbingStairs {
         if (n == 2) {
             return 2L;
         }
-        if (cache.get(n) !=null){
+        if (cache.get(n) != null) {
             return cache.get(n);
         }
 
-        Long reulst = solution(n - 1,cache) + solution(n - 2,cache);
-        cache.put(n,reulst);
+        Long reulst = solution(n - 1, cache) + solution(n - 2, cache);
+        cache.put(n, reulst);
         return reulst;
     }
 
+    /**
+     * 使用通向公式计算
+     * 空间复杂度O(1)
+     * 时间复杂度O(log2n)   主要消耗在几个数学表达式上
+     *
+     * @param n
+     */
+    public static int fib06(int n) {
+        if (n == 1 || n == 2) {
+            return n;
+        } else {
+            double sqrtFive = Math.sqrt(5);
+            n++;
+            double a = Math.pow((1 + sqrtFive) / 2, n);
+            double b = Math.pow((1 - sqrtFive) / 2, n);
+            double result = 1 / sqrtFive * (a - b);
+            return (int) Math.floor(result);
+        }
+    }
+
     public static void main(String[] args) {
-        Map<Long,Long> cache = new HashMap<>();
-        System.out.println(solution(100L,cache));
+        Map<Long, Long> cache = new HashMap<>();
+        System.out.println(solution(100L, cache));
     }
 }
