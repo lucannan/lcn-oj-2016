96fe503c0b6f4367edd389a01692df3950924ed8
samuellcn <812481699@qq.com>
2016-04-14 17:16:54 +0800

    leetcode86  链表的划分

--- src/main/java/CN/EDU/SEU/leetcode/__086PartitionList.java
+++ src/main/java/CN/EDU/SEU/leetcode/__086PartitionList.java
@@ -1,12 +1,16 @@
 package CN.EDU.SEU.leetcode;
 
 import CN.EDU.SEU.极客学院名企面试.链表操作.ListNode;
+import org.junit.Test;
 
 /**
  * Created by LCN on 2016/4/14.
  */
 public class __086PartitionList {
 
+
+    //时间复杂度为O(N)
+    //空间复杂度为O(1)
     public ListNode partition(ListNode head, int x) {
         if (head == null || head.next == null) {
             return head;
@@ -20,12 +24,28 @@ public class __086PartitionList {
         ListNode p = head;
 
         while (p != null) {
-            if (p.val < x){
+            if (p.val < x) {
                 leftTail.next = p;
-
+                leftTail = p;
+            } else {
+                rightTail.next = p;
+                rightTail = p;
             }
+            p = p.next;
         }
+        p = leftTail;
+        p.next = rightHead.next;
 
-        return null;
+        rightTail.next = null;
+        return leftHead.next;
+    }
+
+    @Test
+    public void test(){
+       int [] array = {1,4,3,2,5,2};
+        int x = 3;
+        ListNode head = new ListNode().arrayToList(array);
+        head = partition(head,x);
+        ListNode.printList(head);
     }
 }
