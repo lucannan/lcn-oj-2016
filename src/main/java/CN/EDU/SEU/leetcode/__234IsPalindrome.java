0850957e19a46f1b4fdad2f5761e4017aabe358a
samuellcn <812481699@qq.com>
2016-04-14 16:30:01 +0800

    leetcode234    判断回文链表

---
+++ src/main/java/CN/EDU/SEU/leetcode/__234IsPalindrome.java
@@ -0,0 +1,75 @@
+package CN.EDU.SEU.leetcode;
+
+import CN.EDU.SEU.极客学院名企面试.链表操作.ListNode;
+import org.junit.Test;
+
+/**
+ * 回文链表
+ * Created by LCN on 2016/4/14.
+ */
+public class __234IsPalindrome {
+
+    public int lengthOfList(ListNode head) {
+        ListNode p = head;
+        int n = 0;
+        while (p != null) {
+            p = p.next;
+            n++;
+        }
+        return n;
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = head;
+        ListNode p = pre.next;
+        ListNode next;
+        while (p != null) {
+            next = p.next;
+            p.next = pre;
+            pre = p;
+            p = next;
+        }
+        head.next = null;
+        return pre;
+    }
+
+
+    //时间复杂度O(N)   空间复杂度O(1)
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        int n = lengthOfList(head);
+        int half = n / 2;
+        ListNode leftEnd = head;
+        for (int i = 0; i < half - 1; i++) {
+            leftEnd = leftEnd.next;
+        }
+        ListNode rightStart = leftEnd.next;
+        if (n % 2 != 0) {
+            rightStart = rightStart.next;
+        }
+
+        rightStart = reverseList(rightStart);
+
+        ListNode leftStart = head;
+        for (int i = 1; i <= half; i++) {
+            if (leftStart.value != rightStart.value) {
+                return false;
+            }
+
+            leftStart = leftStart.next;
+            rightStart = rightStart.next;
+        }
+
+        return true;
+    }
+
+    @Test
+    public void test(){
+        Integer[] array1={0,0};
+        ListNode head=new ListNode().arrayToList(array1);
+        System.out.println(isPalindrome(head));
+    }
+}
