1ecab19ecb1e3d062391a70d53e48029129a91e6
samuellcn <812481699@qq.com>
2016-04-23 18:39:06 +0800

    实现一个简单http服务器

--- src/main/java/CN/EDU/SEU/多线程/http服务器/SimpleHttpServer.java
+++ src/main/java/CN/EDU/SEU/多线程/http服务器/SimpleHttpServer.java
@@ -1,7 +1,115 @@
 package CN.EDU.SEU.多线程.http服务器;
 
+
+import com.sun.xml.internal.ws.addressing.WsaTubeHelperImpl;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
+ * 一个基于线程池技术的简单的web服务器
  * Created by LCN on 2016/4/22.
  */
 public class SimpleHttpServer {
+
+    static String basePath;
+
+    static ThreadPool<HttpRequestHandler> threadPool = new DefaultThreadPool<>();
+
+    static ServerSocket serverSocket;
+
+    static int port = 8080;
+
+    public static void setPort(int port) {
+        if (port > 0) {
+            SimpleHttpServer.port = port;
+        }
+    }
+
+    public static void setBasePath(String basePath) {
+        if (basePath != null && new File(basePath).exists() && new File(basePath).isDirectory()) {
+            SimpleHttpServer.basePath = basePath;
+        }
+    }
+
+    public static void start() throws Exception {
+        serverSocket = new ServerSocket(port);
+        Socket socket = null;
+        while ((socket = serverSocket.accept()) != null) {
+            threadPool.excute(new HttpRequestHandler(socket));
+        }
+        serverSocket.close();
+    }
+
+    static class HttpRequestHandler implements Runnable {
+        private Socket socket;
+
+        public HttpRequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            String line = null;
+            BufferedReader br = null;
+            BufferedReader reader = null;
+            PrintWriter out = null;
+            InputStream in = null;
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String header = reader.readLine();
+
+                String filePath = basePath + header.split(" ")[1];
+                out = new PrintWriter(socket.getOutputStream());
+
+                if (filePath.endsWith("jpg") || filePath.endsWith("ico")) {
+                    in = new FileInputStream(filePath);
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int i = 0;
+                    while ((i = in.read()) != -1) {
+                        baos.write(i);
+                    }
+                    byte[] array = baos.toByteArray();
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server:Molly");
+                    out.println("Content-Type: image/jpeg");
+                    out.println("Content-Length：" + array.length);
+                    out.println("");
+                    socket.getOutputStream().write(array, 0, array.length);
+                } else {
+                    br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+                    out = new PrintWriter(socket.getOutputStream());
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server:Molly");
+                    out.println("Content-Type: text/html; charset=UTF-8");
+                    out.println("");
+                    while ((line = br.readLine()) != null) {
+                        out.println(line);
+                    }
+                }
+                out.flush();
+            } catch (Exception ex) {
+                out.println("HTTP/1.1 500");
+                out.println("");
+                out.flush();
+            } finally {
+                close(br, in, reader, out, socket);
+            }
+        }
+    }
+
+
+    private static void close(Closeable... closeables) {
+        if (closeables != null) {
+            for (Closeable closeable : closeables) {
+                try {
+                    closeable.close();
+                } catch (Exception ex) {
+
+                }
+            }
+        }
+    }
 }
